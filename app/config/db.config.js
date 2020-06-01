dd9f8b709991067597d5188740427e9c533590ef
Chikoom <callforcopy@gmail.com>
2020-06-01 17:07:21 +0300

    feat: Heroku DB - Trying to connect using global variable

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,18 +1,34 @@
 const env = require('./env.js');
  
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize(env.database, env.username, env.password, {
-  host: env.host,
-  dialect: env.dialect,
-  operatorsAliases: false,
+// const sequelize = new Sequelize(env.database, env.username, env.password, {
+//   host: env.host,
+//   dialect: env.dialect,
+//   operatorsAliases: false,
  
-  pool: {
-    max: env.max,
-    min: env.pool.min,
-    acquire: env.pool.acquire,
-    idle: env.pool.idle
-  }
-});
+//   pool: {
+//     max: env.max,
+//     min: env.pool.min,
+//     acquire: env.pool.acquire,
+//     idle: env.pool.idle
+//   }
+// });
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect:  'postgres',
+  protocol: 'postgres',
+  port:     match[4],
+  host:     match[3],
+  logging:  true //false
+}
+
+// if (process.env.DATABASE_URL) {
+//   // the application is executed on Heroku ... use the postgres database
+  
+//   })
+// } else {
+  
+// }
  
 const db = {};
  
