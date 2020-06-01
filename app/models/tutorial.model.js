ae60f692fdd278690c8ca55993b7adfb935a7f5e
Chikoom <callforcopy@gmail.com>
2020-06-01 14:01:21 +0300

    feat: Server files updated
    - Created all server files:
    -- db.config for connecting sequelize with heroku postgre DB
    -- tutorial.contrtoller defining CRUD operations api
    -- tutorial.model defining Tutorial DB model
    -- tutorial.routes defining CRUS routes
    
    - Updated server.js file with all app requierments

---
+++ app/models/tutorial.model.js
@@ -0,0 +1,27 @@
+module.exports = (sequelize, Sequelize) => {
+  const Tutorial = sequelize.define('tutorial', {
+    title: {
+      type: Sequelize.STRING
+    },
+    description: {
+      type: Sequelize.STRING
+    },
+    notes: {
+      type: Sequelize.TEXT
+    },
+    published: {
+      type: Sequelize.BOOLEAN
+    },
+    status: {
+      type: Sequelize.ENUM('waiting', 'started', 'done')
+    },
+    link: {
+      type: Sequelize.STRING
+    },
+    publisher: {
+      type: Sequelize.STRING
+    },
+  });
+
+  return Tutorial;
+};
--- server.js
+++ server.js
@@ -1,7 +1,38 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 5000
- 
-app.get('/', (req, res) => res.send('Hello World!'))
- 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:8080"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// sync postgresqi with sequelize using the models
+const db = require("./server/app/models");
+// re-sync db with drop tables for development
+db.sequelize.sync({ force: true }).then(() => {
+  console.log("Drop and re-sync db.");
+});
+//db.sequelize.sync(); // for production
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to myTutorials application." });
+});
+
+require("./server/app/routes/turorial.routes")(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
